dc5163e7471ac7c8f46819f32bbaaad6f24147b3
zhangtaoqd <zhangtao@penavicogw.com>
2015-01-22 07:41:56 +0800

    website

--- App/views.py
+++ App/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def test1(request):
     print('321')
     return render(request, "App/test.html")
+def dealPAjax(request):
+    ls_err = ''
+    l_rtn = {}
+    try:
